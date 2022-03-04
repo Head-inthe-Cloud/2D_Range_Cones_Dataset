a9119894f87f5f65642612c93697d57c4917d87c
Head-inthe-Cloud <583482035@qq.com>
2022-03-03 20:56:09 -0800

    Refactored code

--- plot.py
+++ plot.py
@@ -7,62 +7,60 @@ import matplotlib.animation as animation
 import numpy as np
 import csv
 
+from datasets import extract_from_csv
+
 RMAX = 32.0
 NUM_SHOW = 2   # how many data in the file do you wish to see?
 
 # Specify the data file path
-data_file = 'data/12202021/lidar_data_3m.csv'
-f = open(data_file, 'r', newline='')
-reader = csv.reader(f)
+DATA_FILE = 'data/12202021/lidar_data_3m.csv'
 
-def plot():
-        dataset = []
-        angle = []
-        ran = []
-        intensity = []
-        temp = 0
-        for i, row in enumerate(reader):
-                if i == 0:
-                        continue
-                if i == 1:
-                        temp = float(row[0])
-                if temp != float(row[0]):
-                        dataset.append([angle, ran, intensity])
-                        angle = []
-                        ran = []
-                        intensity = []
-                        temp = float(row[0])
-                angle.append(float(row[1]))
-                ran.append(float(row[2]))
-                intensity.append(float(row[3]))
-        dataset.append([angle, ran, intensity])
-        print(len(dataset))
+
+# Plot a frame with points in the form of [Angle, Rnage] or [Angle, Range, Intensity]
+def plot_ar(frame):
+
+        if np.shape(frame)[1] <= 3:
+                frame = np.transpose(frame)
 
         '''
         fig, ax = plt.subplots(figsize=(10, 6))
         ax.scatter(x = angle, y = ran)
         plt.show()
         '''
+        # fig = plt.figure()
+        # fig.canvas.set_window_title('YDLidar LIDAR Monitor')
+        lidar_polar = plt.subplot(polar=True)
+        lidar_polar = plt.subplot(polar=True)
+        lidar_polar.autoscale_view(True,True,True)
+        lidar_polar.set_rmax(RMAX)
+        lidar_polar.grid(True)
 
-        for i, data in enumerate(dataset):
-                if i >= NUM_SHOW:
-                        break
-                # fig = plt.figure()
-                # fig.canvas.set_window_title('YDLidar LIDAR Monitor')
-                lidar_polar = plt.subplot(polar=True)
-                lidar_polar = plt.subplot(polar=True)
-                lidar_polar.autoscale_view(True,True,True)
-                lidar_polar.set_rmax(RMAX)
-                lidar_polar.grid(True)
+        if len(frame) == 3:
+                lidar_polar.scatter(frame[0], frame[1], c=frame[2], cmap='hsv', alpha=0.95)
+        elif len(frame) == 2:
+                lidar_polar.scatter(frame[0], frame[1], c='r', alpha=0.95)
+        # lidar_polar.scatter(angle, ran, cmap='hsv', alpha=0.95)
+        # lidar_polar.clear()
+        # lidar_polar.scatter(angle, ran, alpha=0.95)
+        plt.show()
 
-                lidar_polar.scatter(data[0], data[1], c=data[2], cmap='hsv', alpha=0.95)
-                # lidar_polar.scatter(angle, ran, cmap='hsv', alpha=0.95)
+def plot_xy(frame1, frame2=None):
+        if len(np.shape(frame1)) == 2 and np.shape(frame1)[1] <= 3:
+                frame1 = np.transpose(frame1)
 
+        plt.plot(frame1[0], frame1[1], 'r.')
 
-
-                # lidar_polar.clear()
-                # lidar_polar.scatter(angle, ran, alpha=0.95)
-                plt.show()
+        if frame2:
+                if np.shape(frame2)[1] <= 3:
+                        frame2 = np.transpose(frame2)
+                plt.plot(frame2[0], frame2[1], 'g.')
+                
+        plt.show()
 
 if __name__ == "__main__":
-        plot()
+        dataset = extract_from_csv(data_file, False)
+
+        for i in range(NUM_SHOW):
+                data = dataset[i]
+                print(np.shape(data))
+                plot_ar(data)
--- template_matching.py
+++ template_matching.py
@@ -1,49 +1,21 @@
 import os
-import csv
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+from plot import plot_ar, plot_xy
+from datasets import extract_from_csv
 
 DATA_FILE = "data/12202021/lidar_data_3m.csv"
 
-raw_data = []
-with open(DATA_FILE) as f:
-    reader = csv.reader(f)
-    time_stamp = 'time_stamp'
-    scan = []
-    # row - [time_stamp, angle, range, intensity]
-    for i, row in enumerate(reader):
-        if i == 0:
-            # print(row)
-            continue
-        if i == 1:
-            time_stamp = row[0]
-        else:
-            if row[0] != time_stamp:
-                time_stamp = row[0]
-                raw_data.append(scan)
-                scan = []
-            scan.append([float(_) for _ in row[1:3]])
 
-raw_data = raw_data[1:]
-
-scan = raw_data[0]
-center_points = []
-for _ in scan:
-    if -0.1 < _[0] < 0.1 and _[1] != 0.0:
-        center_points.append(_)
-
-# [angle, range] -> [x, y]
 # [x, y] -> [angle, range]
-
-
 def toar(point):
     if len(np.shape(point)) == 2:
         return [[math.tanh(p[1]/p[0]), math.sqrt(pow(p[0], 2) + pow(p[1], 2))] for p in point]
     else:
         return [math.tanh(point[1]/point[0]), math.sqrt(pow(point[0], 2) + pow(point[1], 2))]
 
-
+# [angle, range] -> [x, y]
 def toxy(point):
     if len(np.shape(point)) == 2:
         return [[p[1] * math.cos(p[0]), p[1] * math.sin(p[0])] for p in point]
@@ -57,19 +29,48 @@ def plot_points(points):
     plt.show()
 
 
+def extract_center_points():
+    dataset = extract_from_csv(DATA_FILE)
 
-# print(len(center_points))
+    # scan -- [[Angle, Range, Intensity] x N]
+    # Angle vaule range: -pi ~ pi (radians)
+    scan = dataset[0]
+    center_points = []
+
+    # scan -- [[Angle, Range] x N]
+    scan = [point[:2] for point in scan]
+
+    # plot_ar(scan)
+    # extract points at 0 degree with a bias of 0.25 radian
+    # we are only doing this step to limit the number of points for experiment
+    print(np.shape(scan))
+    for point in scan:
+        if -0.25 < point[0] < 0.25 and point[1] != 0.0:
+            center_points.append(point)
+
+    print("Number of Center Points", len(center_points))
+
+    # Examine Center Points
+    # plot_ar(center_points)
+    # plot_xy(toxy(center_points))
+
+    return center_points
+
+
+center_points = extract_center_points()
+
+# working on one center point for now
 center_point = center_points[0]
-# print(center_point)
-
 x, y = toxy(center_point)
+center_point_xy = [x, y]
+
 
 sample_range = 0.23 # suppose to be 3 * standard deviation, now we use cone diameter instead
 sample_gap = 0.05 # how far each sample is from each other
 
 sample_points = []
 
-n = int(sample_range * 2 // 0.05)
+n = int(sample_range * 2 // sample_gap)
 
 temp_x = x - sample_range
 temp_y = y - sample_range
@@ -77,7 +78,9 @@ for i in range(n):
     for j in range(n):
         sample_points.append([temp_x + i * sample_gap, temp_y + j * sample_gap])
 
+plot_xy(center_point_xy, sample_points)
 
+'''
 # change x, y to a, r
 sample_points = [toar(_) for _ in sample_points]
 
@@ -122,7 +125,7 @@ ideal_data = toxy(ideal_data)
 plot_points(ideal_data)
 
 
-'''
+
 # deviation with respect to range
 D = []
 for i in range(len(visible_data)):
@@ -133,8 +136,3 @@ Pr_data_location = sum(D) / len(D)
 
 print(Pr_data_location)
 '''
-
-
-
-
-
