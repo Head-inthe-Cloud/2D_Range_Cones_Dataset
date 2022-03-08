ae05f0bf307eca92f4326fc1916576ec104050a4
Head-inthe-Cloud <583482035@qq.com>
2022-03-07 23:43:41 -0800

    Finished cone template matching

--- plot.py
+++ plot.py
@@ -3,6 +3,10 @@ import ydlidar
 import sys
 from matplotlib.patches import Arc
 import matplotlib.pyplot as plt
+import matplotlib.colors as mcolors
+import matplotlib.cm as cm
+
+import matplotlib.pyplot as plt
 import matplotlib.animation as animation
 import numpy as np
 import csv
@@ -10,17 +14,22 @@ import csv
 from datasets import extract_from_csv
 
 RMAX = 32.0
-NUM_SHOW = 2   # how many data in the file do you wish to see?
+NUM_SHOW = 2  # how many data in the file do you wish to see?
 
 # Specify the data file path
 DATA_FILE = 'data/12202021/lidar_data_3m.csv'
 
 
 # Plot a frame with points in the form of [Angle, Rnage] or [Angle, Range, Intensity]
-def plot_ar(frame):
+def plot_ar(frame1, frame2=None, color=None):
 
+    frames = [frame1]
+    if frame2:
+        frames.append(frame2)
+
+    for frame_idx, frame in enumerate(frames):
         if np.shape(frame)[1] <= 3:
-                frame = np.transpose(frame)
+            frame = np.transpose(frame)
 
         '''
         fig, ax = plt.subplots(figsize=(10, 6))
@@ -29,38 +38,68 @@ def plot_ar(frame):
         '''
         # fig = plt.figure()
         # fig.canvas.set_window_title('YDLidar LIDAR Monitor')
-        lidar_polar = plt.subplot(polar=True)
-        lidar_polar = plt.subplot(polar=True)
-        lidar_polar.autoscale_view(True,True,True)
+        lidar_polar= plt.subplot(polar=True)
+        lidar_polar.autoscale_view(True, True, True)
         lidar_polar.set_rmax(RMAX)
         lidar_polar.grid(True)
 
         if len(frame) == 3:
-                lidar_polar.scatter(frame[0], frame[1], c=frame[2], cmap='hsv', alpha=0.95)
+            lidar_polar.scatter(frame[0], frame[1], c=frame[2], cmap='hsv', alpha=0.95)
         elif len(frame) == 2:
+            if frame_idx == 0:
                 lidar_polar.scatter(frame[0], frame[1], c='r', alpha=0.95)
+            else:
+                lidar_polar.scatter(frame[0], frame[1], c='g', alpha=0.95)
         # lidar_polar.scatter(angle, ran, cmap='hsv', alpha=0.95)
         # lidar_polar.clear()
         # lidar_polar.scatter(angle, ran, alpha=0.95)
-        plt.show()
+
+    plt.show()
+
 
 def plot_xy(frame1, frame2=None):
-        if len(np.shape(frame1)) == 2 and np.shape(frame1)[1] <= 3:
-                frame1 = np.transpose(frame1)
+    if len(np.shape(frame1)) == 2 and np.shape(frame1)[1] <= 3:
+        frame1 = np.transpose(frame1)
 
-        plt.plot(frame1[0], frame1[1], 'r.')
+    fig, ax = plt.subplots()
+    ax.plot(frame1[0], frame1[1], 'r.')
+    ax.plot([3], [0], 'b.')
 
-        if frame2:
-                if np.shape(frame2)[1] <= 3:
-                        frame2 = np.transpose(frame2)
-                plt.plot(frame2[0], frame2[1], 'g.')
-                
-        plt.show()
+    if frame2:
+        if np.shape(frame2)[1] <= 3:
+            frame2 = np.transpose(frame2)
+        ax.plot(frame2[0], frame2[1], 'g.')
+    ax.add_patch(plt.Circle((3, 0), 0.23/2, color='b', fill=False))
+
+    plt.show()
+
+
+def plot_eval(samples, predictions):
+    if np.shape(samples)[1] <= 3:
+        samples = np.transpose(samples)
+
+    lidar_polar = plt.subplot(polar=True)
+    lidar_polar.autoscale_view(True, True, True)
+    lidar_polar.set_rmax(RMAX)
+    lidar_polar.grid(True)
+
+    lidar_polar.scatter(samples[0], samples[1], c=predictions, cmap='hsv', alpha=0.95)
+
+    plt.show()
+
+    normalize = mcolors.Normalize(vmin=predictions.min(), vmax=predictions.max())
+    colormap = cm.jet
+    scalarmappaple = cm.ScalarMappable(norm=normalize, cmap=colormap)
+    scalarmappaple.set_array(predictions)
+    plt.colorbar(scalarmappaple)
+
+    plt.scatter(samples[0], samples[1], c=colormap(normalize(predictions)))
+    plt.show()
 
 if __name__ == "__main__":
-        dataset = extract_from_csv(data_file, False)
+    dataset = extract_from_csv(DATA_FILE, False)
 
-        for i in range(NUM_SHOW):
-                data = dataset[i]
-                print(np.shape(data))
-                plot_ar(data)
+    for i in range(NUM_SHOW):
+        data = dataset[i]
+        print(np.shape(data))
+        plot_ar(data)
--- template_matching.py
+++ template_matching.py
@@ -2,11 +2,18 @@ import os
 import math
 import matplotlib.pyplot as plt
 import numpy as np
-from plot import plot_ar, plot_xy
+from plot import plot_ar, plot_xy, plot_eval
 from datasets import extract_from_csv
 
 DATA_FILE = "data/12202021/lidar_data_3m.csv"
+EPSILON = 10e-6
 
+CONE_DIAMETER = 0.23
+POINTS_PER_DEGREE = 3
+
+# Pr(data) should depend on the state of the vehicle and GPS
+# But for now we are using 1 as its value
+PR_DATA = 1
 
 # [x, y] -> [angle, range]
 def toar(point):
@@ -23,19 +30,54 @@ def toxy(point):
         return [point[1] * math.cos(point[0]), point[1] * math.sin(point[0])]
 
 
+def get_distance(point1, point2):
+    return pow(pow(point1[0] - point2[0], 2) + pow(point1[1] - point2[1], 2), 0.5)
+
+
+def get_k_nearest_points(points, center, k):
+    sorted_points = sorted(points, key=lambda point: get_distance(point, center))
+    return sorted_points[:k]
+
+# returns MSE
+def mean_squared_error(a, b):
+    assert len(a) > 0, "Input length has to be greater than 0"
+    assert len(a) == len(b), "Input arrays must have the same lengths"
+    return sum([pow(diff, 2) for diff in a - b]) / len(a)
+
+
+def get_pr_data_loc(data, samples):
+    result = []
+    data = toxy(data)
+    for point in samples:
+        # get k nearest points
+        visual_angle = 2 * math.tan((CONE_DIAMETER / 2) / point[1]) * 180 / math.pi
+        num_ideal_points = int(visual_angle * POINTS_PER_DEGREE)
+
+        point = toxy(point)
+        nearest_points = get_k_nearest_points(data, point, num_ideal_points)
+        nearest_points = np.array(nearest_points)
+        d_ideal_points = np.zeros(len(nearest_points))
+        d_ideal_points.fill(CONE_DIAMETER / 2)
+        d_nearest_points = [get_distance(p, point) for p in nearest_points]
+
+        mse = mean_squared_error(d_ideal_points, d_nearest_points)
+        result.append(mse)
+    return result
+
 def plot_points(points):
     for p in points:
         plt.scatter(p[0], p[1])
     plt.show()
 
 
-def extract_center_points():
+def extract_data_points():
     dataset = extract_from_csv(DATA_FILE)
 
     # scan -- [[Angle, Range, Intensity] x N]
     # Angle vaule range: -pi ~ pi (radians)
     scan = dataset[0]
-    center_points = []
+
+    data_points = []
 
     # scan -- [[Angle, Range] x N]
     scan = [point[:2] for point in scan]
@@ -46,93 +88,88 @@ def extract_center_points():
     print(np.shape(scan))
     for point in scan:
         if -0.25 < point[0] < 0.25 and point[1] != 0.0:
-            center_points.append(point)
+            data_points.append(point)
 
-    print("Number of Center Points", len(center_points))
+    print("Number of Center Points", len(data_points))
 
     # Examine Center Points
     # plot_ar(center_points)
     # plot_xy(toxy(center_points))
 
-    return center_points
+    return data_points
 
 
-center_points = extract_center_points()
+def evaluate_results(preds, labels):
+    print("Evaluating results ...")
+    assert len(preds) > 0, "Input length must be greater than 0"
+    assert len(preds) == len(labels), "Inputs must have the same lengths"
+    if labels is not np.ndarray:
+        labels = np.array(labels)
+    if preds is not np.ndarray:
+        preds = np.array(preds)
 
-# working on one center point for now
-center_point = center_points[0]
-x, y = toxy(center_point)
-center_point_xy = [x, y]
-
-
-sample_range = 0.23 # suppose to be 3 * standard deviation, now we use cone diameter instead
-sample_gap = 0.05 # how far each sample is from each other
-
-sample_points = []
-
-n = int(sample_range * 2 // sample_gap)
-
-temp_x = x - sample_range
-temp_y = y - sample_range
-for i in range(n):
-    for j in range(n):
-        sample_points.append([temp_x + i * sample_gap, temp_y + j * sample_gap])
-
-plot_xy(center_point_xy, sample_points)
-
-'''
-# change x, y to a, r
-sample_points = [toar(_) for _ in sample_points]
-
-# for each of the sample points
-# Do the following
-
-sample_point = [0.0, 3.0]
-sample_a = sample_point[0]
-sample_r = sample_point[1]
-ConeDiam = 0.23
-
-visual_angle = 2 * math.tan((ConeDiam/2)/sample_r) * 180/math.pi # degree, not radian
-
-# making sure number of data equals to number of ideal data
-visual_angle_range = [sample_a - visual_angle/180 * math.pi / 2, sample_a + visual_angle/180 * math.pi / 2]
-visible_data = [_ for _ in scan if visual_angle_range[0] <= _[0] <= visual_angle_range[1]]
-
-print(len(visible_data))
-
-# TODO
-# Points per degree is not integer, may need further improvement
-Points_per_degree = len(scan) / 360
-# print(Points_per_degree)
-n_template = int(Points_per_degree * visual_angle)
-
-print(n_template)
-
-if n_template % 2 == 0:
-    n_list = [_ for _ in range(0, n_template//2)] + [_ for _ in range(n_template//2 - 1, -1, -1)]
-else:
-    n_list = [_ for _ in range(0, (n_template + 1)//2)] + [_ for _ in range((n_template + 1)//2 - 2, -1, -1)]
-
-ideal_data = []
-visual_angle = visual_angle/180 * math.pi
-
-for i, n in enumerate(n_list):
-    ideal_data.append([visual_angle_range[0] + i * visual_angle/n_template, sample_r - math.sin(n * 180/n_template-1)*(ConeDiam/2)])
+    distance = [get_distance(preds[idx], labels[idx]) for idx in range(len(preds))]
+    mse = mean_squared_error(distance, np.zeros_like(distance))
+    print("MSE =", mse)
 
 
-ideal_data = toxy(ideal_data)
+def get_prediction_from_point():
+    pass
 
-plot_points(ideal_data)
+def main():
+    data_points = extract_data_points()
+    data_points_xy = toxy(data_points)
+
+    # working on one center point for now
+    predictions = []
+
+    sample_range = CONE_DIAMETER  # suppose to be 3 * standard deviation, now we use cone diameter instead
+    sample_gap = 0.05  # how far each sample is from each other
+
+    for data_point in data_points:
+        center_point_xy = toxy([data_point])
+
+        # create sample points
+        sample_points_xy = []
+
+        n = int(sample_range * 2 // sample_gap)
+
+        temp_x = center_point_xy[0][0] - sample_range
+        temp_y = center_point_xy[0][1] - sample_range
+        for i in range(n):
+            for j in range(n):
+                sample_points_xy.append([temp_x + i * sample_gap, temp_y + j * sample_gap])
+
+        sample_points = toar(sample_points_xy)
+
+        '''
+        # Plot sampled points
+        plot_xy(center_point_xy, sample_points_xy)
+        plot_ar(center_point, sample_points)
+        '''
+
+        pr_data_loc = get_pr_data_loc(data_points, sample_points)
+
+        # Pr(Location) should be dependent on standard deviation
+        # But for now we are using constant values
+        pr_loc = np.zeros(len(pr_data_loc))
+        pr_loc.fill(1)
+
+        pr_loc_data = pr_data_loc * pr_loc / PR_DATA
+
+        pred = sample_points[np.argmin(pr_loc_data)]
+        predictions.append(pred)
+
+    print(predictions)
+    data_points.append([0, 0])
+    plot_ar(predictions, data_points)
+    plot_xy(toxy(predictions), data_points_xy)
+    # plot_eval(sample_points, pr_loc_data)
+
+    cone_labels = np.array([[0.0, 3.0]] * len(predictions))
+
+    evaluate_results(predictions, cone_labels)
 
 
-
-# deviation with respect to range
-D = []
-for i in range(len(visible_data)):
-    D.append(abs(visible_data[i][1] - ideal_data[i]))
-
-print(D)
-Pr_data_location = sum(D) / len(D)
-
-print(Pr_data_location)
-'''
+if __name__ == "__main__":
+    main()
