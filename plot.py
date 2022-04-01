a5a9867f2a5933c716e6060d17866cca6a1dca82
Head-inthe-Cloud <583482035@qq.com>
2022-03-31 17:24:13 -0700

    Finished template matching

--- plot.py
+++ plot.py
@@ -12,6 +12,7 @@ import numpy as np
 import csv
 
 from datasets import extract_from_csv
+from utils import toxy
 
 RMAX = 32.0
 NUM_SHOW = 2  # how many data in the file do you wish to see?
@@ -21,61 +22,54 @@ DATA_FILE = 'data/12202021/lidar_data_3m.csv'
 
 
 # Plot a frame with points in the form of [Angle, Rnage] or [Angle, Range, Intensity]
-def plot_ar(frame1, frame2=None, color=None):
+def plot_ar(pred, data=None, color=None):
 
-    frames = [frame1]
-    if frame2:
-        frames.append(frame2)
+    if data:
+        data = np.transpose(data)
+    '''
+    fig, ax = plt.subplots(figsize=(10, 6))
+    ax.scatter(x = angle, y = ran)
+    plt.show()
+    '''
+    # fig = plt.figure()
+    # fig.canvas.set_window_title('YDLidar LIDAR Monitor')
+    lidar_polar= plt.subplot(polar=True)
+    lidar_polar.autoscale_view(True, True, True)
+    lidar_polar.set_rmax(RMAX)
+    lidar_polar.grid(True)
 
-    for frame_idx, frame in enumerate(frames):
-        if np.shape(frame)[1] <= 3:
-            frame = np.transpose(frame)
-
-        '''
-        fig, ax = plt.subplots(figsize=(10, 6))
-        ax.scatter(x = angle, y = ran)
-        plt.show()
-        '''
-        # fig = plt.figure()
-        # fig.canvas.set_window_title('YDLidar LIDAR Monitor')
-        lidar_polar= plt.subplot(polar=True)
-        lidar_polar.autoscale_view(True, True, True)
-        lidar_polar.set_rmax(RMAX)
-        lidar_polar.grid(True)
-
-        if len(frame) == 3:
-            lidar_polar.scatter(frame[0], frame[1], c=frame[2], cmap='hsv', alpha=0.95)
-        elif len(frame) == 2:
-            if frame_idx == 0:
-                lidar_polar.scatter(frame[0], frame[1], c='r', alpha=0.95)
-            else:
-                lidar_polar.scatter(frame[0], frame[1], c='g', alpha=0.95)
-        # lidar_polar.scatter(angle, ran, cmap='hsv', alpha=0.95)
-        # lidar_polar.clear()
-        # lidar_polar.scatter(angle, ran, alpha=0.95)
+    if color:
+        lidar_polar.scatter(data[0], data[1], c=color, cmap='hsv', alpha=0.95)
+    else:
+        lidar_polar.scatter(pred[0], pred[1], c='r', alpha=0.95)
+        lidar_polar.scatter(data[0], data[1], c='g', alpha=0.95)
+    # lidar_polar.scatter(angle, ran, cmap='hsv', alpha=0.95)
+    # lidar_polar.clear()
+    # lidar_polar.scatter(angle, ran, alpha=0.95)
 
     plt.show()
 
 
-def plot_xy(frame1, frame2=None):
-    if len(np.shape(frame1)) == 2 and np.shape(frame1)[1] <= 3:
-        frame1 = np.transpose(frame1)
-
+def plot_xy(frame1, frame2=None, print_circle=False):
     fig, ax = plt.subplots()
+    if print_circle:
+        ax.plot([3], [0], 'b.')
+    frame1 = np.transpose(frame1)
     ax.plot(frame1[0], frame1[1], 'r.')
-    ax.plot([3], [0], 'b.')
 
     if frame2:
         if np.shape(frame2)[1] <= 3:
             frame2 = np.transpose(frame2)
         ax.plot(frame2[0], frame2[1], 'g.')
-    ax.add_patch(plt.Circle((3, 0), 0.23/2, color='b', fill=False))
+    if print_circle:
+        ax.add_patch(plt.Circle((3, 0), 0.23/2, color='b', fill=False))
 
     plt.show()
 
 
 def plot_eval(samples, predictions):
     if np.shape(samples)[1] <= 3:
+        samples_xy = np.transpose(toxy(samples))
         samples = np.transpose(samples)
 
     lidar_polar = plt.subplot(polar=True)
@@ -93,7 +87,7 @@ def plot_eval(samples, predictions):
     scalarmappaple.set_array(predictions)
     plt.colorbar(scalarmappaple)
 
-    plt.scatter(samples[0], samples[1], c=colormap(normalize(predictions)))
+    plt.scatter(samples_xy[0], samples_xy[1], c=colormap(normalize(predictions)))
     plt.show()
 
 if __name__ == "__main__":
--- template_matching.py
+++ template_matching.py
@@ -2,72 +2,78 @@ import os
 import math
 import matplotlib.pyplot as plt
 import numpy as np
+from scipy.stats import norm
 from plot import plot_ar, plot_xy, plot_eval
+from utils import toxy, toar, normalize
 from datasets import extract_from_csv
 
-
-
-# Input: LocX, LocY, Sigma, Lidar data
-# Output: Highest Pr(location | data)
-
-DATA_FILE = "data/12202021/lidar_data_3m.csv"
-EPSILON = 10e-6
+EPSILON = 10e-6    # This number is used to avoid division by 0
 
 CONE_DIAMETER = 0.23
-POINTS_PER_DEGREE = 3
-
-SIGMA =
+POINTS_PER_DEGREE = 1      # This number should be 3 based on previous calculation, but 1 seem to be more accurate in practice
 
 # Pr(data) should depend on the state of the vehicle and GPS
 # But for now we are using 1 as its value
 PR_DATA = 1
 
-# [x, y] -> [angle, range]
-def toar(point):
-    if len(np.shape(point)) == 2:
-        return [[math.tanh(p[1]/p[0]), math.sqrt(pow(p[0], 2) + pow(p[1], 2))] for p in point]
-    else:
-        return [math.tanh(point[1]/point[0]), math.sqrt(pow(point[0], 2) + pow(point[1], 2))]
 
-# [angle, range] -> [x, y]
-def toxy(point):
-    if len(np.shape(point)) == 2:
-        return [[p[1] * math.cos(p[0]), p[1] * math.sin(p[0])] for p in point]
-    else:
-        return [point[1] * math.cos(point[0]), point[1] * math.sin(point[0])]
-
-
+# Returns the distance between two points, point => (x, y)
 def get_distance(point1, point2):
     return pow(pow(point1[0] - point2[0], 2) + pow(point1[1] - point2[1], 2), 0.5)
 
-def get_k_nearest_points(points, center, k):
-    sorted_points = sorted(points, key=lambda point: get_distance(point, center))
+
+# points is an array of points in xy form, center is a point in xy form, k is an integer
+def get_k_nearest_points(data, center, k):
+    sorted_points = sorted(data, key=lambda point: get_distance(point, center))
     return sorted_points[:k]
 
-# returns MSE
+
+# returns MSE of arrays a and b
 def mean_squared_error(a, b):
     assert len(a) > 0, "Input length has to be greater than 0"
     assert len(a) == len(b), "Input arrays must have the same lengths"
     return sum([pow(diff, 2) for diff in a - b]) / len(a)
 
 
+# returns pr(data | location), the result has the same length as input samples
 def get_pr_data_loc(data, samples):
     result = []
-    data = toxy(data)
-    for point in samples:
+    for s_point in samples:
         # get k nearest points
-        visual_angle = 2 * math.tan((CONE_DIAMETER / 2) / point[1]) * 180 / math.pi
+        visual_angle = 2 * math.tan((CONE_DIAMETER / 2) / s_point[1]) * 180 / math.pi
         num_ideal_points = int(visual_angle * POINTS_PER_DEGREE)
 
-        point = toxy(point)
-        nearest_points = get_k_nearest_points(data, point, num_ideal_points)
+        s_point = toxy(s_point)
+        nearest_points = get_k_nearest_points(data, s_point, num_ideal_points)
         nearest_points = np.array(nearest_points)
+
         d_ideal_points = np.zeros(len(nearest_points))
         d_ideal_points.fill(CONE_DIAMETER / 2)
-        d_nearest_points = [get_distance(p, point) for p in nearest_points]
+        d_nearest_points = [get_distance(n_point, s_point) for n_point in nearest_points]
 
         mse = mean_squared_error(d_ideal_points, d_nearest_points)
-        result.append(mse)
+
+        result.append(1 / mse)
+    return result
+
+
+def get_pr_loc(LocX, LocY, sigma, sampled_points_xy):
+    # pr_loc is calculated using methods in this website: https://www.askpython.com/python/normal-distribution
+    # The pr(location) is calculated by subtracting the probability from 1:
+    #     Upper limit(prob at distance) - lower limit(prob at 0)
+    #     Multiply by 2 (it was only half of the probability)
+    #     1 - the above result
+
+    distances = [get_distance([LocX, LocY], sampled_point) for sampled_point in sampled_points_xy]
+    # Plot normal distribution
+    # pdf = norm.pdf(distances, loc=0, scale=sigma)
+    # plt.plot(distances, pdf)
+    # plt.xlabel("Distance From Center")
+    # plt.ylabel("Probability Density")
+    # plt.show()
+
+    lower_limit = norm(loc=0, scale=sigma).cdf(0)
+    result = [1 - (norm(loc=0, scale=sigma).cdf(distance) - lower_limit) * 2 for distance in distances]
     return result
 
 def plot_points(points):
@@ -76,33 +82,17 @@ def plot_points(points):
     plt.show()
 
 
-def extract_data_points():
-    dataset = extract_from_csv(DATA_FILE)
-
+def extract_data_points(data_path):
+    dataset = extract_from_csv(data_path)
     # scan -- [[Angle, Range, Intensity] x N]
     # Angle vaule range: -pi ~ pi (radians)
-    scan = dataset[0]
-
-    data_points = []
+    scan = []
 
     # scan -- [[Angle, Range] x N]
-    scan = [point[:2] for point in scan]
-
-    # plot_ar(scan)
-    # extract points at 0 degree with a bias of 0.25 radian
-    # we are only doing this step to limit the number of points for experiment
-    print(np.shape(scan))
-    for point in scan:
-        if -0.25 < point[0] < 0.25 and point[1] != 0.0:
-            data_points.append(point)
-
-    print("Number of Center Points", len(data_points))
-
-    # Examine Center Points
-    # plot_ar(center_points)
-    # plot_xy(toxy(center_points))
-
-    return data_points
+    for data_point in dataset[0]:
+        if data_point[1] > 0:
+            scan.append(data_point[:2])
+    return scan
 
 
 def evaluate_results(preds, labels):
@@ -119,65 +109,66 @@ def evaluate_results(preds, labels):
     print("MSE =", mse)
 
 
-def get_prediction_from_point():
-    pass
+def get_new_sigma(new_center, points, pr_loc_data, sigma, threshold):
+    if type(points) is not np.ndarray:
+        points = np.array(points)
+    if type(pr_loc_data) is not np.ndarray:
+        pr_loc_data = np.array(pr_loc_data)
+    possible_points = points[np.where(pr_loc_data > threshold)[0]]
+    distances = np.sort(np.array([get_distance(new_center, point) for point in possible_points]))
+    min_distance = distances[1] # skips the first element, which is the distance to center point itself
+    return min_distance / (3 * sigma)
 
-def template_matching(LocX, LocY, Sigma, lidar_data):
-    pass
 
-def main():
-    data_points = extract_data_points()
+# Input: LocX, LocY, Sigma, Lidar data
+# Output: New_LocX, New_LocY, New_Sigma, PrDetect
+def template_matching(LocX, LocY, sigma, lidar_data, eval_mode=False):
+    # data_points = extract_data_points()
+    data_points = lidar_data
     data_points_xy = toxy(data_points)
 
-    # working on one center point for now
-    predictions = []
-
-    sample_range = CONE_DIAMETER  # suppose to be 3 * standard deviation, now we use cone diameter instead
+    sample_range = 3 * sigma
     sample_gap = 0.05  # how far each sample is from each other
 
-    for data_point in data_points:
-        center_point_xy = toxy([data_point])
+    center_point_xy = [LocX, LocY]
 
-        # create sample points
-        sample_points_xy = []
+    # create sample points
+    sample_points_xy = []
 
-        n = int(sample_range * 2 // sample_gap)
+    n = int(sample_range * 2 / sample_gap)
 
-        temp_x = center_point_xy[0][0] - sample_range
-        temp_y = center_point_xy[0][1] - sample_range
-        for i in range(n):
-            for j in range(n):
-                sample_points_xy.append([temp_x + i * sample_gap, temp_y + j * sample_gap])
+    temp_x = center_point_xy[0] - sample_range
+    temp_y = center_point_xy[1] - sample_range
+    for i in range(n):
+        for j in range(n):
+            sample_points_xy.append([temp_x + i * sample_gap, temp_y + j * sample_gap])
 
-        sample_points = toar(sample_points_xy)
+    sample_points = toar(sample_points_xy)
 
-        '''
-        # Plot sampled points
-        plot_xy(center_point_xy, sample_points_xy)
-        plot_ar(center_point, sample_points)
-        '''
+    pr_data_loc = np.array(get_pr_data_loc(data_points_xy, sample_points))
+    pr_loc = np.array(get_pr_loc(LocX, LocY, sigma, sample_points_xy))
+    pr_loc_data = normalize(pr_data_loc * pr_loc / PR_DATA)
+    pr_detect = np.amax(pr_loc_data)
+    pred = toxy(sample_points[np.argmax(pr_loc_data)])
+    new_LocX, new_LocY = pred
 
-        pr_data_loc = get_pr_data_loc(data_points, sample_points)
+    new_sigma = get_new_sigma(pred, sample_points_xy, pr_loc_data, sigma, 0.5)
 
-        # Pr(Location) should be dependent on standard deviation
-        # But for now we are using constant values
-        pr_loc = np.zeros(len(pr_data_loc))
-        pr_loc.fill(1)
+    if eval_mode:
+        print(pred)
+        print(pr_data_loc)
+        data_points.append([0, 0])
+        # plot_eval(sample_points, pr_data_loc)
+        # plot_eval(sample_points, pr_loc)
+        plot_ar(pred, data_points)
+        plot_xy(toxy(pred), data_points_xy, True)
+        plot_eval(sample_points, pr_loc_data)
+        cone_label = [[0.0, 3.0]]
 
-        pr_loc_data = pr_data_loc * pr_loc / PR_DATA
+        evaluate_results([pred], cone_label)
 
-        pred = sample_points[np.argmin(pr_loc_data)]
-        predictions.append(pred)
-
-    print(predictions)
-    data_points.append([0, 0])
-    plot_ar(predictions, data_points)
-    plot_xy(toxy(predictions), data_points_xy)
-    # plot_eval(sample_points, pr_loc_data)
-
-    cone_labels = np.array([[0.0, 3.0]] * len(predictions))
-
-    evaluate_results(predictions, cone_labels)
+    return new_LocX, new_LocY, new_sigma, pr_detect
 
 if __name__ == "__main__":
-    main()
+    # template_matching(3, 0, 0.35, extract_data_points("data/12202021/lidar_data_3m.csv"), True)
+    template_matching(2.0, 0, 0.35, extract_data_points("data/12202021/lidar_data_noisy_3m_2.csv"))
